71f007f8b6672ffd2f59906a789e3fca705fac17
Phuc Nguyen Bao <phuc_nguyenbao@datahouse.com>
2026-02-15 01:09:22 +0700

    fix tienlen client server url for remote access

--- src/tienlen/server/client/src/socket/events.js
+++ src/tienlen/server/client/src/socket/events.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const SERVER_URL = 'http://localhost:3001'
+const SERVER_URL = `${window.location.protocol}//${window.location.hostname}:3001`
 
 const suitBase = { S: 0, H: 13, D: 26, C: 39 }
 const rankToOffset = { A: 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7, '9': 8, '10': 9, J: 10, Q: 11, K: 12 }
